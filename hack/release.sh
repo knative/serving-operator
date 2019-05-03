c22387f57400d6a4e22bceb159cabf72464fe38a
Jim Crossley <jim@crossleys.org>
2019-05-03 16:18:51 -0400

    Override manifest filename now that quay.io images available
    Only affects installation via OLM

--- hack/release.sh
+++ hack/release.sh
@@ -4,11 +4,13 @@ set -ex
 
 pushd $(dirname "$0")/..
 
+readonly HUB_ORG="${HUB_ORG:-"openshift-knative"}"
+
 IMAGE=$(basename $(pwd))
 
 eval VERSION=v$(grep Version version/version.go | awk '{print $3}')
-operator-sdk build quay.io/openshift-knative/$IMAGE:$VERSION
-docker push quay.io/openshift-knative/$IMAGE:$VERSION
+operator-sdk build quay.io/$HUB_ORG/$IMAGE:$VERSION
+docker push quay.io/$HUB_ORG/$IMAGE:$VERSION
 git tag -f $VERSION
 git push --tags --force
 
