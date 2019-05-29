d2ef61deb72dfac110b9a96f1e34abb130fdb703
Jim Crossley <jim@crossleys.org>
2019-05-29 14:12:21 -0400

    Use the images from our fork

--- hack/publish-ci-images.sh
+++ hack/publish-ci-images.sh
@@ -4,7 +4,7 @@ set -ex
 
 version=$1
 
-for image in {knative-serving-queue,knative-serving-activator,knative-serving-autoscaler,knative-serving-controller,knative-serving-webhook}; do
+for image in {knative-serving-queue,knative-serving-activator,knative-serving-autoscaler,knative-serving-controller,knative-serving-webhook,knative-serving-certmanager,knative-serving-istio}; do
   src=registry.svc.ci.openshift.org/openshift/knative-$version:$image
   tgt=quay.io/openshift-knative/$image:$version
   docker pull $src
