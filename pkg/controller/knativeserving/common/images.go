9bf5ba029f5bd30379bd5a28e94a046ea0c96593
Thomas Shafer <tshafer@google.com>
2019-07-12 09:39:39 -0700

    Relocate the transformation logic to each respective common library. (#67)

--- pkg/controller/knativeserving/common/images.go
+++ pkg/controller/knativeserving/common/images.go
@@ -19,10 +19,14 @@ import (
 	"fmt"
 	"strings"
 
+	mf "github.com/jcrossley3/manifestival"
+
 	"github.com/go-logr/logr"
 	servingv1alpha1 "github.com/knative/serving-operator/pkg/apis/serving/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 	caching "knative.dev/caching/pkg/apis/caching/v1alpha1"
 )
 
@@ -31,8 +35,59 @@ var (
 	containerNameVariable = "${NAME}"
 )
 
-// UpdateDeploymentImage updates the image of the deployment with a new registry and tag
-func UpdateDeploymentImage(deployment *appsv1.Deployment, registry *servingv1alpha1.Registry, log logr.Logger) {
+func DeploymentTransform(scheme *runtime.Scheme, instance *servingv1alpha1.KnativeServing, log logr.Logger) mf.Transformer {
+	return func(u *unstructured.Unstructured) error {
+		// Update the deployment with the new registry and tag
+		if u.GetKind() == "Deployment" {
+			return updateDeployment(scheme, instance, u, log)
+		}
+		return nil
+	}
+}
+
+func ImageTransform(scheme *runtime.Scheme, instance *servingv1alpha1.KnativeServing, log logr.Logger) mf.Transformer {
+	return func(u *unstructured.Unstructured) error {
+		// Update the image with the new registry and tag
+		if u.GetAPIVersion() == "caching.internal.knative.dev/v1alpha1" && u.GetKind() == "Image" {
+			return updateCachingImage(scheme, instance, u)
+		}
+		return nil
+	}
+}
+
+func updateDeployment(scheme *runtime.Scheme, instance *servingv1alpha1.KnativeServing, u *unstructured.Unstructured, log logr.Logger) error {
+	var deployment = &appsv1.Deployment{}
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, deployment)
+	if err != nil {
+		log.Error(err, "Error converting Unstructured to Deployment", "unstructured", u, "deployment", deployment)
+		return err
+	}
+
+	registry := instance.Spec.Registry
+	log.V(1).Info("Updating Deployment", "name", u.GetName(), "registry", registry)
+
+	updateDeploymentImage(deployment, &registry, log)
+	err = updateUnstructured(u, deployment, log)
+	if err != nil {
+		return err
+	}
+
+	log.V(1).Info("Finished conversion", "name", u.GetName(), "unstructured", u.Object)
+	return nil
+}
+
+func updateUnstructured(u *unstructured.Unstructured, obj interface{}, log logr.Logger) error {
+	unstructuredObj, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&obj)
+	if err != nil {
+		log.Error(err, "Error converting obj to Unstructured", "unstructured", u, "obj", obj)
+		return err
+	}
+	u.SetUnstructuredContent(unstructuredObj)
+	return nil
+}
+
+// updateDeploymentImage updates the image of the deployment with a new registry and tag
+func updateDeploymentImage(deployment *appsv1.Deployment, registry *servingv1alpha1.Registry, log logr.Logger) {
 	containers := deployment.Spec.Template.Spec.Containers
 	for index := range containers {
 		container := &containers[index]
@@ -41,11 +96,31 @@ func UpdateDeploymentImage(deployment *appsv1.Deployment, registry *servingv1alp
 			updateContainer(container, newImage, log)
 		}
 	}
-	log.V(1).Info("Finished updating images", "deployment", deployment.GetName())
+	log.V(1).Info("Finished updating images", "name", deployment.GetName(), "containers", deployment.Spec.Template.Spec.Containers)
 }
 
-// UpdateImageSpec updates the image of a with a new registry and tag
-func UpdateImageSpec(image *caching.Image, registry *servingv1alpha1.Registry, log logr.Logger) {
+func updateCachingImage(scheme *runtime.Scheme, instance *servingv1alpha1.KnativeServing, u *unstructured.Unstructured) error {
+	var image = &caching.Image{}
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, image)
+	if err != nil {
+		log.Error(err, "Error converting Unstructured to Image", "unstructured", u, "image", image)
+		return err
+	}
+
+	registry := instance.Spec.Registry
+	log.V(1).Info("Updating Image", "name", u.GetName(), "registry", registry)
+
+	updateImageSpec(image, &registry, log)
+	err = updateUnstructured(u, image, log)
+	if err != nil {
+		return err
+	}
+	log.V(1).Info("Finished conversion", "name", u.GetName(), "unstructured", u.Object)
+	return nil
+}
+
+// updateImageSpec updates the image of a with a new registry and tag
+func updateImageSpec(image *caching.Image, registry *servingv1alpha1.Registry, log logr.Logger) {
 	newImage := getNewImage(registry, image.Name)
 	if newImage != "" {
 		log.V(1).Info(fmt.Sprintf("Updating image from: %v, to: %v", image.Spec.Image, newImage))
--- pkg/controller/knativeserving/common/images_test.go
+++ pkg/controller/knativeserving/common/images_test.go
@@ -3,9 +3,12 @@ package common
 import (
 	"testing"
 
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
 	servingv1alpha1 "github.com/knative/serving-operator/pkg/apis/serving/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
 	caching "knative.dev/caching/pkg/apis/caching/v1alpha1"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 
@@ -92,15 +95,42 @@ var updateDeploymentImageTests = []updateDeploymentImageTest{
 	},
 }
 
-func TestUpdateDeploymentImage(t *testing.T) {
+func TestDeploymentTransform(t *testing.T) {
 	for _, tt := range updateDeploymentImageTests {
 		t.Run(tt.name, func(t *testing.T) {
-			runUpdateDeploymentImageTest(t, tt)
+			runDeploymentTransformTest(t, &tt)
 		})
 	}
 }
-func runUpdateDeploymentImageTest(t *testing.T, tt updateDeploymentImageTest) {
+func runDeploymentTransformTest(t *testing.T, tt *updateDeploymentImageTest) {
+	log := logf.Log.WithName(tt.name)
+	logf.SetLogger(logf.ZapLogger(true))
+	testScheme := runtime.NewScheme()
+	unstructuredDeployment := makeUnstructuredDeployment(t, tt, testScheme)
+	instance := &servingv1alpha1.KnativeServing{
+		Spec: servingv1alpha1.KnativeServingSpec{
+			Registry: tt.registry,
+		},
+	}
+	deploymentTransform := DeploymentTransform(testScheme, instance, log)
+	deploymentTransform(&unstructuredDeployment)
+	validateUnstructedDeploymentChanged(t, tt, &unstructuredDeployment)
+}
+
+func validateUnstructedDeploymentChanged(t *testing.T, tt *updateDeploymentImageTest, u *unstructured.Unstructured) {
+	var deployment = &appsv1.Deployment{}
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, deployment)
+	assertEqual(t, err, nil)
+	for i, expected := range tt.expected {
+		assertEqual(t, deployment.Spec.Template.Spec.Containers[i].Image, expected)
+	}
+}
+
+func makeUnstructuredDeployment(t *testing.T, tt *updateDeploymentImageTest, scheme *runtime.Scheme) unstructured.Unstructured {
 	deployment := appsv1.Deployment{
+		TypeMeta: metav1.TypeMeta{
+			Kind: "Deployment",
+		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name: tt.name,
 		},
@@ -112,13 +142,12 @@ func runUpdateDeploymentImageTest(t *testing.T, tt updateDeploymentImageTest) {
 			},
 		},
 	}
-	log := logf.Log.WithName(tt.name)
-	logf.SetLogger(logf.ZapLogger(true))
-
-	UpdateDeploymentImage(&deployment, &tt.registry, log)
-
-	for i, expected := range tt.expected {
-		assertEqual(t, deployment.Spec.Template.Spec.Containers[i].Image, expected)
+	unstructuredDeployment, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&deployment)
+	if err != nil {
+		t.Fatalf("Could not create unstructured deployment object: %v, err: %v", unstructuredDeployment, err)
+	}
+	return unstructured.Unstructured{
+		Object: unstructuredDeployment,
 	}
 }
 
@@ -140,15 +169,42 @@ var updateImageSpecTests = []updateImageSpecTest{
 	},
 }
 
-func TestUpdateImageSpec(t *testing.T) {
+func TestImageTransform(t *testing.T) {
 	for _, tt := range updateImageSpecTests {
 		t.Run(tt.name, func(t *testing.T) {
-			runUpdateImageSpecTest(t, tt)
+			runImageTransformTest(t, &tt)
 		})
 	}
 }
-func runUpdateImageSpecTest(t *testing.T, tt updateImageSpecTest) {
+func runImageTransformTest(t *testing.T, tt *updateImageSpecTest) {
+	log := logf.Log.WithName(tt.name)
+	logf.SetLogger(logf.ZapLogger(true))
+
+	testScheme := runtime.NewScheme()
+	unstructuredImage := makeUnstructuredImage(t, tt, testScheme)
+	instance := &servingv1alpha1.KnativeServing{
+		Spec: servingv1alpha1.KnativeServingSpec{
+			Registry: tt.registry,
+		},
+	}
+	imageTransform := ImageTransform(testScheme, instance, log)
+	imageTransform(&unstructuredImage)
+	validateUnstructedImageChanged(t, tt, &unstructuredImage)
+}
+
+func validateUnstructedImageChanged(t *testing.T, tt *updateImageSpecTest, u *unstructured.Unstructured) {
+	var image = &caching.Image{}
+	err := runtime.DefaultUnstructuredConverter.FromUnstructured(u.Object, image)
+	assertEqual(t, err, nil)
+	assertEqual(t, image.Spec.Image, tt.expected)
+}
+
+func makeUnstructuredImage(t *testing.T, tt *updateImageSpecTest, scheme *runtime.Scheme) unstructured.Unstructured {
 	image := caching.Image{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "caching.internal.knative.dev/v1alpha1",
+			Kind:       "Image",
+		},
 		ObjectMeta: metav1.ObjectMeta{
 			Name: tt.name,
 		},
@@ -156,15 +212,16 @@ func runUpdateImageSpecTest(t *testing.T, tt updateImageSpecTest) {
 			Image: tt.in,
 		},
 	}
-	log := logf.Log.WithName(tt.name)
-	logf.SetLogger(logf.ZapLogger(true))
-
-	UpdateImageSpec(&image, &tt.registry, log)
-
-	assertEqual(t, image.Spec.Image, tt.expected)
+	unstructuredDeployment, err := runtime.DefaultUnstructuredConverter.ToUnstructured(&image)
+	if err != nil {
+		t.Fatalf("Could not create unstructured deployment object: %v, err: %v", unstructuredDeployment, err)
+	}
+	return unstructured.Unstructured{
+		Object: unstructuredDeployment,
+	}
 }
 
-func assertEqual(t *testing.T, actual, expected string) {
+func assertEqual(t *testing.T, actual, expected interface{}) {
 	if actual == expected {
 		return
 	}
