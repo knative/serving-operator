684dfde8326cd893bcf1e308e9f447b748e91772
mattmoor-sockpuppet <mattmoor+sockpuppet@google.com>
2019-08-09 09:24:05 -0700

    golang format tools (#134)
    Produced via:
      `gofmt -s -w $(find -path './vendor' -prune -o -type f -name '*.go' -print))`
      `goimports -w $(find -name '*.go' | grep -v vendor)`
    /assign @mattmoor

--- test/resources/knativeserving.go
+++ test/resources/knativeserving.go
@@ -38,7 +38,7 @@ const (
 	// Interval specifies the time between two polls.
 	Interval = 10 * time.Second
 	// Timeout specifies the timeout for the function PollImmediate to reach a certain status.
-	Timeout  = 5 * time.Minute
+	Timeout = 5 * time.Minute
 )
 
 // WaitForKnativeServingState polls the status of the KnativeServing called name
@@ -65,7 +65,7 @@ func WaitForKnativeServingState(clients servingv1alpha1.KnativeServingInterface,
 func CreateKnativeServing(clients servingv1alpha1.KnativeServingInterface, names test.ResourceNames) (*v1alpha1.KnativeServing, error) {
 	ks := &v1alpha1.KnativeServing{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: names.KnativeServing,
+			Name:      names.KnativeServing,
 			Namespace: names.Namespace,
 		},
 	}
